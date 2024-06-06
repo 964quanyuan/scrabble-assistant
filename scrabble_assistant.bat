1d22fa5269c0529c16f078cd66b0d243e58a496f
jerryyuan-umd <80700616+jerryyuan-umd@users.noreply.github.com>
2024-06-06 11:52:37 -0400

    Update scrabble_assistant.bat

--- scrabble_assistant.bat
+++ scrabble_assistant.bat
@@ -1,8 +1,8 @@
 @echo off
 echo hello mutherfucker!
 start /B python -c "from sounds.play_audio import greet; greet()"
-start /B python "C:\Users\31415\Documents\Scrabble\observe.py"
+start /B python observe.py
 start "" "C:\Program Files\e2eSoft\iVCAM\iVCAM.exe"
-start /min explorer "C:\Users\31415\Videos\iVCam"
+start /min explorer "C:\Users\[username]\Videos\iVCam"
 timeout /t 2 /nobreak >nul
 python -c "from sounds.play_audio import rick_entry; rick_entry()"
